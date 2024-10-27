3138e967a733563a98c123de7699d354638922bc
Soul pc <119080077+soulpc@users.noreply.github.com>
2024-10-27 21:29:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,9 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+from aiohttp import web
+from plugins import web_server
+PORT = environ.get("PORT", "8050")
 
 class Bot(Client):
 
@@ -42,6 +45,11 @@ class Bot(Client):
         self.username = '@' + me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
+        
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()       
 
     async def stop(self, *args):
         await super().stop()
